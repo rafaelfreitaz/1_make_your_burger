da469f72fde6d1549dea0b0bcdacc02db806dfb5
rafaelfreitaz <rafaelfreitaz@outlook.com>
2023-08-17 21:10:58 -0300

    17-07-23
    finalizado!

---
+++ src/views/Pedidos.vue
@@ -0,0 +1,21 @@
+<template>
+ <Banner />
+  <div class="main-container">
+    <h1>Gerenciar pedidos:</h1>
+    <Dashboard />
+  </div>
+</template>
+
+<script>
+import Dashboard from '../components/Dashboard.vue';
+import Banner from '../components/Banner.vue';
+
+export default {
+  name: "Pedidos",
+  components: {
+    Dashboard,
+    Banner
+  }
+}
+</script>
+
